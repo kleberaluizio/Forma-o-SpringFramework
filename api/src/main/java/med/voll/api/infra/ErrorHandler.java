ea331d2710ed941955b8e1908961ac6ad658bc66
kleberaluizio <kleberaluizio@gmail.com>
2023-08-12 15:07:31 -0300

    Creating error handler for erro 400

--- api/src/main/java/med/voll/api/infra/ErrorHandler.java
+++ api/src/main/java/med/voll/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +13,19 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handleError404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException exception){
+        var errors = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationErrorsData::new).toList());
+
+    }
+
+    private record ValidationErrorsData(String field, String message){
+        public ValidationErrorsData(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+
     }
 }
