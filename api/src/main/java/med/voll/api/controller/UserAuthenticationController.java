75a2061ccea61dad3a2612fc874481203e0a2213
kleberaluizio <kleberaluizio@gmail.com>
2023-08-13 21:37:09 -0300

    Adding Spring Security to this app project

---
+++ api/src/main/java/med/voll/api/controller/UserAuthenticationController.java
@@ -0,0 +1,25 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.user.UserAuthenticationData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/login")
+public class UserAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @GetMapping
+    public ResponseEntity executeLogin(@RequestBody @Valid UserAuthenticationData userData){
+        var token = new UsernamePasswordAuthenticationToken(userData.login(),userData.password());
+        var authentication = manager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ api/src/main/java/med/voll/api/domain/user/UserAuthenticationData.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.user;
+
+public record UserAuthenticationData(String login, String password) {
+}
