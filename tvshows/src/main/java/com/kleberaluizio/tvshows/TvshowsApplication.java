7eb9c3d5abf6cee9f1a2537eb88a8c5f1c533137
kleberaluizio <kleberaluizio@gmail.com>
2023-09-18 10:12:50 -0300

    Adding service class

--- tvshows/src/main/java/com/kleberaluizio/tvshows/TvshowsApplication.java
+++ tvshows/src/main/java/com/kleberaluizio/tvshows/TvshowsApplication.java
@@ -1,8 +1,11 @@
 package com.kleberaluizio.tvshows;
 
+import com.kleberaluizio.tvshows.service.ConsumeAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class TvshowsApplication implements CommandLineRunner {
@@ -13,6 +16,24 @@ public class TvshowsApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto spring sem web");
+
+		Scanner leitura = new Scanner(System.in);
+		String search = "";
+
+		while(!search.equalsIgnoreCase("sair")) {
+			System.out.println("Qual filme você gostaria de buscar? ");
+			search = leitura.nextLine();
+
+			if (search.equalsIgnoreCase("sair")) {
+				break;
+			}
+			String address = "https://www.omdbapi.com/?t=" + search.replace(" ", "+") + "&apikey=638e10ca";
+
+			ConsumeAPI consumeAPI = new ConsumeAPI();
+
+			var json = consumeAPI.obtainData(address);
+
+			System.out.println(json);
+		}
 	}
 }
